7e9243d664cd67b5ec9218b59d4e480407fd6576
Andrew Michaud <dev@drew.life>
2014-02-05 15:14:47 -0800

    bounds checking, started on setSquare

--- data.hs
+++ data.hs
@@ -18,15 +18,30 @@ data SudokuBoard = SudokuBoard [[SquareValue]] deriving (Show)
 
 -- Creates empty Sudoku board.
 emptyBoard :: SudokuBoard
-emptyBoard = SudokuBoard [replicate 9 x | x <- (replicate 9 Nothing)]
+emptyBoard = SudokuBoard [replicate 9 x | x <- (replicate 9 (Nothing))]
 
 -- Get the value in a particular square of a Sudoku board.
-getBoardValue :: SudokuBoard -> Int -> Int -> SquareValue
-getBoardValue (SudokuBoard board) rowIndex colIndex = value
+getBoardValue :: SudokuBoard -> Int -> Int -> Maybe SquareValue
+getBoardValue (SudokuBoard board) rowIndex colIndex
+    -- Check if out of bounds.
+    | rowIndex > 8 || rowIndex < 0 = Just Nothing
+    | colIndex > 8 || colIndex < 0 = Just Nothing
+    -- Otherwise return value.
+    | otherwise    = Just value
     where
         row   = board !! rowIndex
         value = row !! colIndex
 
 -- Returns a SudokuBoard with the value at the two indexes modified.
--- setBoardValue :: SudokuBoard -> Int -> Int -> Int -> SudokuBoard
--- setBoardValue (SudokuBoard board) rowIndex colIndex newValue =
+setBoardValue :: SudokuBoard -> Int -> Int -> Int -> SudokuBoard
+setBoardValue (SudokuBoard board) rowIndex colIndex newValue 
+    | rowIndex > 8 || rowIndex < 0 = SudokuBoard board
+    | colIndex > 8 || colIndex < 0 = SudokuBoard board
+    | oldValue == newValue         = SudokuBoard board
+    where
+	row	 = board !! rowIndex
+	oldValue = row !! colIndex 
+
+instance Show SudokuBoard board where
+    SudokuBoard board = newBoard
+
