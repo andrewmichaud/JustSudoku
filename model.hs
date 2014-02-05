6714214f234cc058843b151666af70a9a46ec1e6
Andrew Michaud <andrewjmichaud@gmail.com>
2014-02-05 04:58:07 -0800

    Created data.hs to hold types for SquareValue and SudokuBoard and some methods relating to making those work properly.

--- model.hs
+++ model.hs
@@ -6,11 +6,33 @@ import qualified Data.Vector as V
 import Data.Maybe
 import qualified Data.List
 
+data Value = Number Int deriving (Show)
+
+-- data Board = SudokuBoard [Int] [Int] [Int] [Int] [Int] [Int] [Int] [Int] [Int]
+
+-- Main function
 main = do
-    -- get vars
-    vars <- getLine
 
-    let varsArray = words vars
+    -- Get all lines
+    s <- getContents
 
-    print varsArray
+
+    let 
+        -- Split contents into an array of lines.
+        allLines = lines s
+
+        -- Split each line into an array of words.
+        listOLines = [words line | line <- allLines]
+
+
+    print listOLines
+
+
+
+process s = show s 
+    -- This code loads a sudoku file into the model.
+    -- TODO split this into controller.
+    -- TODO don't hardcode in a file.
+
+
 
