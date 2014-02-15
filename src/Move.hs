2b0df33339d3e38bdec205ff1b2fea2cca31745f
Andrew Michaud <dev@drew.life>
2014-02-15 06:47:12 -0800

    made a bunch of progress towards a working controller

---
+++ src/Move.hs
@@ -0,0 +1,11 @@
+-- Andrew Michaud
+-- 02/15/14
+-- Contains Move type, used for the player to say what they would like to do.
+
+module Move
+( Move(..)
+) where
+
+-- Types of moves the player can make.
+data Move = Get String String | Set String String String | Check | Quit
+
