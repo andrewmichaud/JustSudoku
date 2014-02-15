789cd2e253ce5b15d56fd69884feb806be127cae
Andrew Michaud <andrewjmichaud@gmail.com>
2014-02-15 03:15:51 -0800

    wrote new converting methods in SudokuBoard.hs and reorganized the file. Also, wrote parsing methods in controller.hs to start to allow a control loop.

--- src/SudokuBoard.hs
+++ src/SudokuBoard.hs
@@ -2,73 +2,90 @@
 -- 02/05/15
 -- Data and other definitions needed for Sudoku.
 
-module SudokuBoard
-( SudokuBoard
+module SudokuBoard 
+( SqVal
 , Square
-, SqVal
-, toVal
+, SudokuBoard
+, Location
+, toLocation
+, toSquare
 , emptyBoard
-, testRow
+, prettyPrint
+, getBoardValue
 , setBoardValue
 , checkBoard
-, prettyPrint
 ) where
 
 import Data.Maybe
-import qualified Data.List
+import Text.Read
 
-data SqVal = V1 | V2 | V3 | V4 | V5 | V6 | V7 | V8 | V9 deriving (Eq, Ord, Enum, Show)
+-- Types declared.
 
-toVal :: Int -> Maybe SqVal
-toVal 1 = Just V1
-toVal 2 = Just V2
-toVal 3 = Just V3
-toVal 4 = Just V4
-toVal 5 = Just V5
-toVal 6 = Just V6
-toVal 7 = Just V7
-toVal 8 = Just V8
-toVal 9 = Just V9
-toVal _ = Nothing
+-- Location on a SudokuGrid
+data Location = Loc Int Int deriving (Eq, Show)
 
 -- Type for Sudoku square value.
 data Square = Empty | Val SqVal deriving (Eq)
 
--- Test constructor.
-testRow :: [Int] -> [Square]
-testRow nums = answer
-    where
-        vals       = map (toVal) nums
-        answer = map (filterFunc) vals
-
-filterFunc :: Maybe SqVal -> Square
-filterFunc val = if isJust val then Val (fromJust val) else Empty
-
--- Show either the number, or "N" for nothing.
-instance Show Square where
-    show (Val value) = show value
-    show (Empty)    = "N"
+-- Value of a Square, can be 1-9
+data SqVal = V1 | V2 | V3 | V4 | V5 | V6 | V7 | V8 | V9 deriving (Eq, Ord, Enum, Show)
 
 -- Type for Sudoku board.
 data SudokuBoard = SudokuBoard [[Square]] deriving (Show)
 
--- Check if a pair of Squares are equal.
-checkPair :: Square -> Square -> Bool
-checkPair (Val squareA) (Val squareB) = (squareA == squareB)
-checkPair _ _ = False
+-- Methods visible from this module.
 
--- Check a list of Squares and returns a list of pairs that are equal.
-checkList :: [Square] -> [(Square, Square)]
-checkList (x:xs)
-    | tail xs == [] = if checkPair x (head xs) then [(x, (head xs))] else []
-    | otherwise     = answer
+-- Convert Int to Square
+toSquare :: String -> Maybe Square
+toSquare "0" = Just Empty
+toSquare "1" = Just $ Val V1
+toSquare "2" = Just $ Val V2
+toSquare "3" = Just $ Val V3
+toSquare "4" = Just $ Val V4
+toSquare "5" = Just $ Val V5
+toSquare "6" = Just $ Val V6
+toSquare "7" = Just $ Val V7
+toSquare "8" = Just $ Val V8
+toSquare "9" = Just $ Val V9
+toSquare _ = Nothing
+
+-- Create Location
+toLocation :: String -> String -> Maybe Location
+toLocation rowStr colStr 
+    | row == Nothing || col == Nothing = Nothing
+    | otherwise                        = Just $ Loc (fromJust row) (fromJust col)
     where
-        headPairs   = [(x, y) | y <- xs, checkPair x y]
-        answer      = headPairs ++ checkList xs
+        row = toIndex rowStr
+        col = toIndex colStr
+
+-- Creates empty Sudoku board.
+emptyBoard :: SudokuBoard
+emptyBoard = SudokuBoard [replicate 9 x | x <- (replicate 9 (Empty))]
+
+-- Get the value in a particular square of a Sudoku board.
+getBoardValue :: SudokuBoard -> Location -> Maybe Square
+getBoardValue (SudokuBoard board) (Loc rowIndex colIndex)
+    
+    -- Otherwise return value
+    | otherwise                    = Just value
+    where
+        row   = board !! rowIndex
+        value = row !! colIndex
+
+-- Returns a SudokuBoard with the value at the two indices modified.
+setBoardValue :: SudokuBoard -> Location -> Square -> Maybe SudokuBoard
+setBoardValue (SudokuBoard board) (Loc rowIndex colIndex) newSquare
+    
+    -- Return new board if everything seems all right.
+    | otherwise                    = Just $ SudokuBoard newBoard
+    where
         
--- Retrieves a specified column from a 2D list.
-getColumn :: [[a]] -> Int -> [a]
-getColumn list index = [row !! index| row <- list]
+        -- Create new row.
+        oldRow    = board !! rowIndex
+        newRow    = take colIndex oldRow ++ [newSquare] ++ drop (colIndex + 1) oldRow
+
+	-- Create new board.
+        newBoard  = take rowIndex board ++ [newRow] ++ drop (colIndex + 1) board
 
 -- Checks if a SudokuBoard is currently valid or not.
 -- Checks all rows, columns, and subgrids with checkList and returns a 
@@ -80,18 +97,6 @@ checkBoard (SudokuBoard board) = allPairs
         columns  = [getColumn board index | index <- [0..9]]
         colPairs = concat $ map checkList board
         allPairs = colPairs ++ rowPairs
-
-
--- Given an array and a spacer, uses the spacer to separate
--- the first three from the middle three rows and the middle
--- three from the last three rows.
-sudokuSpacer :: a -> [a] -> [a]
-sudokuSpacer spacer array = result
-    where 
-        first3 = take 3 array
-        mid3   = take 3 (drop 3 array)
-        last3  = drop 6 array
-        result = first3 ++ [spacer] ++ mid3 ++ [spacer] ++ last3
 
 -- Show for SudokuBoard, prints it nicely.
 prettyPrint :: SudokuBoard -> String
@@ -112,59 +117,53 @@ prettyPrint (SudokuBoard board) = niceBoard
         
         -- Join array into newline-separated string.
         niceBoard     = unlines separated
- 
--- Creates empty Sudoku board.
-emptyBoard :: SudokuBoard
-emptyBoard = SudokuBoard [replicate 9 x | x <- (replicate 9 (Empty))]
 
--- Get the value in a particular square of a Sudoku board.
-getBoardValue :: SudokuBoard -> Int -> Int -> Maybe Square
-getBoardValue (SudokuBoard board) rowIndex colIndex
-    
--- Check if out of bounds.
-    | rowIndex > 8 || rowIndex < 0 = Nothing
-    | colIndex > 8 || colIndex < 0 = Nothing
-    
-    -- Otherwise return value.
-    | otherwise                    = Just value
+-- Hidden in this module.
+
+-- Show either the number, or "N" for nothing.
+instance Show Square where
+    show (Val value) = show value
+    show (Empty)    = "N"
+
+-- Turn an int into an index value.
+toIndex :: String -> Maybe Int
+toIndex "0" = Just 0
+toIndex "1" = Just 1
+toIndex "2" = Just 2
+toIndex "3" = Just 3
+toIndex "4" = Just 4
+toIndex "5" = Just 5
+toIndex "6" = Just 6
+toIndex "7" = Just 7
+toIndex "8" = Just 8
+toIndex _   = Nothing
+
+-- Check if a pair of Squares are equal.
+checkPair :: Square -> Square -> Bool
+checkPair (Val squareA) (Val squareB) = (squareA == squareB)
+checkPair _ _ = False
+
+-- Check a list of Squares and returns a list of pairs that are equal.
+checkList :: [Square] -> [(Square, Square)]
+checkList (x:xs)
+    | tail xs == [] = if checkPair x (head xs) then [(x, (head xs))] else []
+    | otherwise     = answer
     where
-        row   = board !! rowIndex
-        value = row !! colIndex
+        headPairs   = [(x, y) | y <- xs, checkPair x y]
+        answer      = headPairs ++ checkList xs
+        
+-- Retrieves a specified column from a 2D list.
+getColumn :: [[a]] -> Int -> [a]
+getColumn list index = [row !! index| row <- list]
 
--- Returns a SudokuBoard with the value at the two indices erased.
-eraseBoardValue :: SudokuBoard -> Int -> Int -> Maybe SudokuBoard
-eraseBoardValue (SudokuBoard board) rowIndex colIndex
-    -- Check if out of bounds.
-    | rowIndex > 8 || rowIndex < 0 = Nothing
-    | colIndex > 8 || colIndex < 0 = Nothing
-    
-    -- Return new board if everything seems all right.
-    | otherwise                    = Just $ SudokuBoard newBoard
-    where
-        oldRow    = board !! rowIndex
-        -- Create new row.
-        newRow    = take colIndex oldRow ++ [Empty] ++ drop (colIndex + 1) oldRow
-
-	-- Create new board.
-        newBoard  = take rowIndex board ++ [newRow] ++ drop (colIndex + 1) board
-
--- Take squares? Return errors?
-
--- Returns a SudokuBoard with the value at the two indices modified.
-setBoardValue :: SudokuBoard -> Int -> Int -> Square -> Maybe SudokuBoard
-setBoardValue (SudokuBoard board) rowIndex colIndex newSquare
-    
-    -- Check if out of bounds.
-    | rowIndex > 8 || rowIndex < 0 = Nothing
-    | colIndex > 8 || colIndex < 0 = Nothing
-    
-    -- Return new board if everything seems all right.
-    | otherwise                    = Just $ SudokuBoard newBoard
-    where
-        oldRow    = board !! rowIndex
-        -- Create new row.
-        newRow    = take colIndex oldRow ++ [newSquare] ++ drop (colIndex + 1) oldRow
-
-	-- Create new board.
-        newBoard  = take rowIndex board ++ [newRow] ++ drop (colIndex + 1) board
+-- Given an array and a spacer, uses the spacer to separate
+-- the first three from the middle three rows and the middle
+-- three from the last three rows.
+sudokuSpacer :: a -> [a] -> [a]
+sudokuSpacer spacer array = result
+    where 
+        first3 = take 3 array
+        mid3   = take 3 (drop 3 array)
+        last3  = drop 6 array
+        result = first3 ++ [spacer] ++ mid3 ++ [spacer] ++ last3
         
--- src/controller.hs
+++ src/controller.hs
@@ -10,13 +10,17 @@ import System.Environment
 -- For file and other IO.
 import System.IO
 
+import Data.Maybe
+
 import SudokuBoard
 
--- Copied (with permission) from a friend's code.
+-- Styled (with permission) after a friend's code.
 repl :: (Show a, Show c) => (a -> b -> Either a c) -> (String -> Maybe b) -> a -> IO ()
 repl f parse init = do
-    z <- getLine
-    maybe fail succeed (parse z) where
+    
+    -- Grab input line.
+    line <- getLine
+    maybe fail succeed (parse line) where
         fail = do
             print "Parse error. Try again."
             repl f parse init
@@ -25,10 +29,31 @@ repl f parse init = do
             print newstate
             repl f parse $ either id (const init) newstate
 
+-- Parse a move
+-- Also styled after a friend's code.
+parseMove :: String -> Maybe (Location, Square)
+-- Take a move in the form "rcs", where "r" indicates the row index, "c" the column index,
+-- and "s" the value to set the square at those indices.
+parseMove (row:col:sq)
+    | loc    == Nothing = Nothing
+    | square == Nothing = Nothing
+    | otherwise         = Just (fromJust loc, fromJust square)
+    where loc       = parseLocation [row] [col]
+          square    = parseSquare sq
+
+-- Turn an Int into a square (or nothing).
+-- 0 is turned into Empty.
+parseSquare :: String -> Maybe Square
+parseSquare intString = toSquare intString
+
+-- Turn two ints into a Location
+parseLocation :: String -> String -> Maybe Location
+parseLocation rowString colString = toLocation rowString colString
+
 main = do
 
     -- Command line arguments.
-    --(filename:args) <- getArgs
+    arguments <- getArgs
 
     -- Read from file.
     --contents <- readFile filename
