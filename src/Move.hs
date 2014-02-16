71eb752342f4c8cb296760940d26204d5d077a76
Andrew Michaud <dev@drew.life>
2014-02-16 15:43:48 -0800

    reorganized files somewhat.

--- src/Move.hs
+++ src/Move.hs
@@ -4,8 +4,36 @@
 
 module Move
 ( Move(..)
+, MoveError(..)
 ) where
+
+-- For custom errors.
+import Control.Monad.Error
+
+-- For fromJust and other things.
+import Data.Maybe
+
+-- For Read.Maybe
+import Text.Read
+
+import SudokuBoard
 
 -- Types of moves the player can make.
 data Move = Set String String String | Check | Quit
+
+-- Kinds of move errors that can occur.
+data MoveError = NaNError String | OutOfBoundsError Int Int | InvalidValueError Int | 
+                 OtherError String | InvalidBoardError [(Location, Location)]
+
+-- For when the error has an error?
+instance Error MoveError where
+    noMsg  = OtherError "KERNEL PANIC"
+    strMsg = OtherError
+instance Show MoveError where
+    show (NaNError value)            = "Value " ++ value ++ " is not a number."
+    show (OutOfBoundsError row col)  = "Square (" ++ show row ++ ", " ++ show col 
+                                                 ++ " is out of bounds."
+    show (InvalidValueError value)   = "Value " ++ show value ++ " is invalid."
+    show (InvalidBoardError squares) = "Board is invalid. Invalid squares: " ++ show squares
+    show (OtherError string)         = "General error: " ++ string
 
