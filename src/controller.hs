5c6bf18ba5f6fb329424b4fcad174a3eac014e57
Andrew Michaud <andrewjmichaud@gmail.com>
2014-02-16 01:51:51 -0800

    control loop functional.  need to fix bug with setting and finish implementing checkBoard

--- src/controller.hs
+++ src/controller.hs
@@ -24,32 +24,46 @@ import SudokuBoard
 import Move
 
 -- Styled (with permission) after a friend's code.
-repl :: (Show a, Show c) => (a -> b -> Either a c) -> (String -> Maybe b) -> a -> IO ()
+repl :: (SudokuBoard -> Move -> Either MoveError SudokuBoard) -> (String -> Maybe Move) -> SudokuBoard -> IO ()
 repl f parse init = do
     
     -- Grab input line.
     line <- getLine
+
+    -- Parse the line as a move.
     let possibleMove = parseMove line
 
+    -- This handles the cases of failing to parse and successfully (maybe) parsing.
     maybe fail succeed (parse line) where
+
+        -- If we fail to parse, say so and recurse with init.
         fail = do
-            print "Parse error. Try again."
+            putStrLn "Unable to parse move. Try again.\n"
             repl f parse init
+
+        -- If nothing immediately goes wrong, we take the a and attempt to apply it
+        -- to receive a new state.
         succeed a = do
-            let newstate = f init a
-            print newstate
-            repl f parse $ either id (const init) newstate
+
+            -- Obtain and print newstate.
+            let newstate  = f init a
+            putStrLn $ either show prettyPrint newstate
+
+            -- Print prompt.
+            putStrLn "What would you like to do?\n"
+
+            -- either has type (a -> c) -> (b -> c) -> Either a b -> c
+            -- I'm using the Left value of Either as an error, and the right value as an actual
+            -- value.  So, here we do the following.
+            -- If newstate is an error, we return init. const init is a function that takes a value
+            -- and throws it away, and returns init.
+            -- If newstate is not an error, we return it. id just takes newstate and returns it.
+            -- The end result is that we have a new value to recurse with repl.
+            repl f parse $ either (const init) id newstate
 
 -- Parse a move
 -- Also styled after a friend's code.
 parseMove :: String -> Maybe Move
-
--- Get value at a row and column.
-parseMove ('g':r:c)
-    | loc == Nothing = Nothing
-    | otherwise      = Just $ Get [r] c
-    where
-        loc = parseLocation [r] c
 
 -- Set value at row and column to value.
 parseMove ('s':r:c:v)
@@ -98,18 +112,6 @@ instance Show MoveError where
 -- Try to processs a move.
 move :: SudokuBoard -> Move -> Either MoveError SudokuBoard
 
--- Get the value. Return error if desired value is out of bounds or not a number.
--- In no event modify the board.
-move board (Get row col)
-    | rowInt   == Nothing = Left $ NaNError row
-    | colInt   == Nothing = Left $ NaNError col
-    | location == Nothing = Left $ OutOfBoundsError (fromJust rowInt) (fromJust colInt)
-    | otherwise           = Right $ board
-    where
-        location = parseLocation row col
-        rowInt   = readMaybe row
-        colInt   = readMaybe col
-
 -- Set value to board. If an error occurs, the value will not be set. If no error occurs, the
 -- requested value will be set.
 move board (Set row col val)
@@ -149,11 +151,19 @@ main = do
     -- Read from file.
     --contents <- readFile filename
     
+    -- Game header.
+    putStrLn "This is Sudoku-Linux version 0.1\n"
+
+    -- Print original board.
+    putStrLn "This is the board\n"
 
     let board   = emptyBoard
-        pp      = prettyPrint board
+        
+    putStrLn $ prettyPrint board
 
-    putStrLn pp
-
-    --repl move parseMove board
+    -- Print prompt.
+    putStrLn "What would you like to do?\n"
+    
+    -- Enter control loop.
+    repl move parseMove board
 
