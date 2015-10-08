5059d77e8f0b0b2962cb26cac7d7c40d9c46a6ef
Andrew Michaud <dev@drew.life>
2015-10-07 22:36:47 -0700

    more tests, ideally finally fix hpc-coveralls

--- test/Util/OtherSpec.hs
+++ test/Util/OtherSpec.hs
@@ -8,6 +8,9 @@ main = hspec spec
 
 spec :: Spec
 spec = do
+    let int0 = 0::Int
+    let int1 = 1::Int
+    let int3 = 3::Int
     describe "sToIntRange" $ do
         it "should return Nothing if the range is empty" $
             sToIntRange "a" [] `shouldBe` Nothing
@@ -22,9 +25,11 @@ spec = do
             sToIntRange "2" [1] `shouldBe` Nothing
 
     describe "getMonadicRow" $
-        it "should work with Just 1" $
-            getMonadicRow 1 (Just 1) `shouldBe` Just [1]
+        it "should work with Just n" $ do
+            getMonadicRow int1 (Just int1) `shouldBe` Just [1]
+            getMonadicRow int3 (Just int1) `shouldBe` Just [1,1,1]
+            getMonadicRow int0 (Just int1) `shouldBe` Just []
 
     describe "getMonadicGrid" $
-        it "should work with Just 1" $
-            getMonadicGrid 1 (Just 1) `shouldBe` Just [[1]]
+        it "should work with Just n" $
+            getMonadicGrid int1 (Just int1) `shouldBe` Just [[1]]
