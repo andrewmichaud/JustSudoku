fc3105550187e38e716f34ac4fe80dfa924ba2f6
Andrew Michaud <dev@drew.life>
2015-10-08 11:20:48 -0700

    add the skeleton of new tests, and remove getMonadicRow, which I realize is just a slower alias of replicateM

---
+++ test/Sudoku/Control/MoveSpec.hs
@@ -0,0 +1,10 @@
+module Sudoku.Control.MoveSpec (main, spec) where
+
+import Test.Hspec
+import Sudoku.Control.Move
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = return ()
--- test/Util/OtherSpec.hs
+++ test/Util/OtherSpec.hs
@@ -10,7 +10,6 @@ spec :: Spec
 spec = do
     let int0 = 0::Int
     let int1 = 1::Int
-    let int3 = 3::Int
     describe "sToIntRange" $ do
         it "should return Nothing if the range is empty" $
             sToIntRange "a" [] `shouldBe` Nothing
@@ -23,13 +22,6 @@ spec = do
 
         it "should return Nothing if the string is not in the range" $
             sToIntRange "2" [1] `shouldBe` Nothing
-
-    describe "getMonadicRow" $ do
-        it "should work with Just n" $ do
-            getMonadicRow int1 (Just int1) `shouldBe` Just [1]
-            getMonadicRow int3 (Just int1) `shouldBe` Just [1,1,1]
-        it "should return an empty list of the right type given 0" $
-            getMonadicRow int0 (Just int1) `shouldBe` Just []
 
     describe "getMonadicGrid" $ do
         it "should work with Just n" $
